a1bf03c5be445694804d8e15f761f9e7ef0f3a15
Buksbuny <so18-45@ukr.net>
2017-11-10 15:46:33 +0000

    add new

--- js/index.js
+++ js/index.js
@@ -1,9 +1,6 @@
-let a,b,result;
-a=0; 
-b=1;
-result=b;
 
-for (var i = 0; i < 100; i++)
+let a = 0,b = 1, result = b;
+for (let i = 0; i < 100; i++)
 {
     document.write(result + "<br>");
     result = a+b;
